6da1ccf9ce26decd2f2806d426b19c6ab991522c
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-02-13 14:41:58 -0200

    Possibilidades de onde pode ser inserido código JavaScript

---
+++ js/meuscript.js
@@ -0,0 +1,6 @@
+/*Arquivo externo JavaScript para Alterar um parágrafo do documento
+HTML '5.Onde-Inserir-Codigo-JavaScript.html'*/
+
+function alterarParagrafoQuatro() {
+  document.getElementById("paragrafo-quatro").innerHTML = "Parágrafo Quatro Alterado Com Success!";
+}
