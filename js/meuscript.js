81155788dcb27ac9947fbb9a44b918c3e7adcc4e
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-07-27 10:16:19 -0300

    Primeiro arquivo do livro Programação Javascript

--- js/meuscript.js
+++
@@ -1,6 +0,0 @@
-/*Arquivo externo JavaScript para Alterar um parágrafo do documento
-HTML '5.Onde-Inserir-Codigo-JavaScript.html'*/
-
-function alterarParagrafoQuatro() {
-  document.getElementById("paragrafo-quatro").innerHTML = "Parágrafo Quatro Alterado Com Success!";
-}
