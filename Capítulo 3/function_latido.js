555ce4c4f39599c6f2df075d06bc8536318bb7c5
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-08-06 20:03:59 -0300

    Primeira função de latido

---
+++ Capítulo 3/function_latido.js
@@ -0,0 +1,12 @@
+function latir(nome, peso) {
+	if(peso > 20) {
+		console.log(nome + " diz WOOF WOOF!");
+	} else {
+		console.log(nome + " diz woof woof!");
+	}
+}
+
+latir("rover", 23);
+latir("spot", 13);
+latir("spike", 53);
+latir("lady", 17);
