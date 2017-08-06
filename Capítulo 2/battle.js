872c827269cf6aaba342f754840fd6f8c9e99748
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2017-08-06 17:30:18 -0300

    Quarto arquivo do livro Programação Javascript 2

--- Capítulo 2/battle.js
+++ Capítulo 2/battle.js
@@ -2,9 +2,9 @@ var palpite;
 var totalPalpites = 0;
 var totalAcertos = 0;
 var afundou = false;
-var local1 = 3;
-var local2 = 4;
-var local3 = 5;
+var local1 = Math.floor(Math.random() * 5);
+var local2 = local1 + 1;
+var local3 = local2 + 1;
 
 while(afundou == false) {
    palpite = prompt("Digite a posicao do navio 0-5:");
